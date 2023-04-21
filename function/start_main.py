9166f4ed64644eb4c97e579cd746a4242542c0fe
zsbcn <15139569980@163.com>
2023-04-22 03:17:51 +0800

    优化保存文件功能-禁止保存为已打开的文件；打开功能优化-打开已存在的文件时自动切换为当前页，并提示是否加载新的文件

--- function/start_main.py
+++ function/start_main.py
@@ -8,6 +8,7 @@
 # version    ：python 3.10
 # Description：
 """
+import os
 import sys
 
 from PyQt6.QtWidgets import QApplication, QFileDialog, QInputDialog, QMessageBox
@@ -28,7 +29,7 @@ class MyMain(ThonnyMainWindow):
             if len(file_name) == 0:
                 file_name = f'New File {len(self.tab_fileNames) + 1}'
             self.new_tab_callback(file_name)
-            self.save_file_temp(file_name, '')
+            self.save_file2temp(file_name, '')
 
     def open_file_callback(self, event):
         """
@@ -37,19 +38,26 @@ class MyMain(ThonnyMainWindow):
         """
         file_name, _ = QFileDialog.getOpenFileName(self, '打开文件')
         if file_name in self.tab_fileNames:
+            self.tab_widget.setCurrentWidget(self.tab_objects[self.tab_fileNames.index(file_name)])
             reply = QMessageBox.information(self, '提示', '文件已打开，是否重新加载？',
                                             QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No,
                                             QMessageBox.StandardButton.No)
             if reply == QMessageBox.StandardButton.Yes:
-                with open(file_name, mode='r', encoding='utf-8') as read_file:
-                    self.tab_widget.currentWidget().setText(read_file.read())
-                    self.save_file_temp(file_name, read_file.read())
-            self.tab_widget.setCurrentWidget(self.tab_objects[self.tab_fileNames.index(file_name)])
-        else:
+                self.load_file(file_name)
+        elif file_name:
+            self.load_file(file_name, new_tab=True)
+
+    def load_file(self, file_name, new_tab=False):
+        """
+        加载文件
+        :param file_name: 加载文件的路径
+        :param new_tab: 是否新建Tab页
+        """
+        if new_tab:
             self.new_tab_callback(file_name)
-            with open(file_name, mode='r', encoding='utf-8') as read_file:
-                self.tab_widget.currentWidget().setText(read_file.read())
-                self.save_file_temp(file_name, read_file.read())
+        with open(file_name, mode='r', encoding='utf-8') as read_file:
+            self.tab_widget.currentWidget().setText(read_file.read())
+            self.save_file2temp(file_name, read_file.read())
 
     def save_file_callback(self, event):
         """
@@ -57,27 +65,11 @@ class MyMain(ThonnyMainWindow):
         :param event:
         """
         current_file_name = self.tab_widget.tabText(self.tab_widget.currentIndex())
+        current_content = self.tab_objects[self.tab_widget.currentIndex()].text()
         temp_file_name = f'{get_temp_name(current_file_name)}.temp'
-        if current_file_name.startswith('New File'):
-            file_path, _ = QFileDialog.getSaveFileName(self, '保存文件', '.')
-            if file_path:
-                self.save_file_temp(temp_file_name,
-                                    self.tab_widget.currentWidget().text())  # 保存临时文件
-                self.tab_widget.setTabText(self.tab_widget.currentIndex(), file_path)  # 更新Tab的名字
-        # if os.path.exists(temp_file_name):
-        #     if is_different(temp_file_name, self.tab_objects[self.tab_widget.currentIndex()].text()):
-        #         file_path, _ = QFileDialog.getSaveFileName(self, '保存文件', '.')
-        #         self.save_file_temp(temp_file_name,
-        #                             self.tab_widget.currentWidget().text())  # 保存临时文件
-        #         self.tab_widget.setTabText(self.tab_widget.currentIndex(), file_path)  # 更新Tab的名字
-
-        # if file_path and os.path.exists():
-        # if current_file_name.startswith('New File') or (
-        #         os.path.exists(temp_file_name) and is_different(temp_file_name, current_file_name)):
-        #     file_path, _ = QFileDialog.getSaveFileName(self, '保存文件', '.')
-        #     with open(file_path, mode='w', encoding='utf-8') as save_file:
-        #         save_file.write(self.tab_objects[self.tab_widget.currentIndex()].text())
-        #     self.tab_widget.setTabText(self.tab_widget.currentIndex(), file_path)
+        if current_file_name.startswith('New File') \
+                or (os.path.exists(temp_file_name) and is_different(temp_file_name, current_content)):
+            self.update_file(current_content)
 
     def close_tab_callback(self, index):
         """
@@ -99,7 +91,7 @@ class MyMain(ThonnyMainWindow):
         self.tab_widget.addTab(self.tab_objects[tab_nums], tab_name)
         self.tab_widget.setCurrentWidget(self.tab_objects[tab_nums])
 
-    def save_file_temp(self, temp_name, temp_content):
+    def save_file2temp(self, temp_name, temp_content):
         """
         保存临时文件的回调函数
         :param temp_name: 临时文件的名字
@@ -107,6 +99,20 @@ class MyMain(ThonnyMainWindow):
         """
         with open(f'{get_temp_name(temp_name)}.temp', mode='w', encoding='utf-8') as temp_file:
             temp_file.write(temp_content)
+
+    def update_file(self, new_content: str):
+        file_path, _ = QFileDialog.getOpenFileName(self, '保存文件')
+        if file_path in self.tab_fileNames:
+            QMessageBox.warning(self, '提示', '文件已打开，无法保存')
+        elif file_path:
+            with open(file_path, mode='w', encoding='utf-8') as save_file:
+                save_file.write(new_content)
+            self.tab_widget.setTabText(self.tab_widget.currentIndex(), file_path)
+            self.save_file2temp(file_path, new_content)
+            self.tab_fileNames[self.tab_widget.currentIndex()] = file_path
+
+    def reload_file(self):
+        pass
 
 
 if __name__ == '__main__':
